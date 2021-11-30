a13dab254820784897f6dd1bdfdfa737d30a41ee
sudal <gaesudal@gmail.com>
2021-11-30 18:06:31 +0900

    Initial commit

---
+++ src/main/java/com/fastcampus/programming/dmaker/controller/DMakerController.java
@@ -0,0 +1,48 @@
+package com.fastcampus.programming.dmaker.controller;
+
+import com.fastcampus.programming.dmaker.dto.CreateDeveloper;
+import com.fastcampus.programming.dmaker.dto.DeveloperDetailDto;
+import com.fastcampus.programming.dmaker.dto.DeveloperDto;
+import com.fastcampus.programming.dmaker.service.DMakerService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class DMakerController {
+
+    private final DMakerService dMakerService;
+
+    @GetMapping("/developers")
+    public List<DeveloperDto> getAllDevelopers(){
+
+        log.info("GET /developers HTTP/1.1");
+
+        return dMakerService.getAllDevelopers();
+    }
+
+    @GetMapping("/developer/{memberId}")
+    public DeveloperDetailDto getDeveloperDetail(
+            @PathVariable String memberId
+    ) {
+        log.info("GET /developers HTTP/1.1");
+
+        return dMakerService.getDeveloperDetail(memberId);
+    }
+
+
+    @PostMapping("/create-developer")
+    public CreateDeveloper.Response createDeveloper(@Valid @RequestBody CreateDeveloper.Request request){
+
+        log.info("request : {}", request);
+
+        return dMakerService.createDeveloper(request);
+    }
+}
